ae4862f9ee901f919d058f2b552dfe2a6d474a9d
Vladislav Ponomarev <vlad11ponomarev@gmail.com>
2022-06-30 22:25:20 +0300

    first step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { nanoid } from "nanoid";
+
+
+const INITIAL_STATE = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson'},
+    { id: 'id-2', name: 'Hermione Kline'},
+    { id: 'id-3', name: 'Eden Clements'},
+    { id: 'id-4', name: 'Annie Copeland'},
+  ],
+  name: '',
 };
+
+export class App extends Component {
+  state = INITIAL_STATE;
+  
+  handleSubmit = event => {
+    event.preventDefault();
+    const { name } = this.state;
+    if (!name) return;
+    const contact = {
+      id: nanoid(),
+      name,
+    };
+    this.setState({
+      contacts: [...this.state.contacts, contact],
+      name: '',
+    });
+  }
+  
+  handleChange = event => {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+    render() {
+      const { contacts, name } = this.state;
+      return (
+        <div>
+          <h1>Phone book</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Add contact</button>
+          </form>
+          <h2>Contacts</h2>
+          <ul>
+            {contacts.map(contact => (
+              <li key={contact.id}>{contact.name}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  
+  }
