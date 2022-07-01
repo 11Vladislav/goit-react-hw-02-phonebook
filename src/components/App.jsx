9309bd7cd255eb56facd62eed53df7e4fc849e9d
Vladislav Ponomarev <vlad11ponomarev@gmail.com>
2022-07-01 18:09:54 +0300

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,55 +4,73 @@ import { nanoid } from "nanoid";
 
 const INITIAL_STATE = {
   contacts: [
-    { id: 'id-1', name: 'Rosie Simpson'},
-    { id: 'id-2', name: 'Hermione Kline'},
-    { id: 'id-3', name: 'Eden Clements'},
-    { id: 'id-4', name: 'Annie Copeland'},
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
   name: '',
+  number: ''
 };
 
 export class App extends Component {
   state = INITIAL_STATE;
   
-  handleSubmit = event => {
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
-    const { name } = this.state;
-    if (!name) return;
+    const { name, number } = this.state;
+    if (name === '' || number === '') {
+      return;
+    }
     const contact = {
       id: nanoid(),
       name,
+      number
     };
     this.setState({
       contacts: [...this.state.contacts, contact],
       name: '',
+      number: ''
     });
   }
-  
-  handleChange = event => {
-    this.setState({
-      name: event.target.value,
-    });
-  }
-
     render() {
-      const { contacts, name } = this.state;
+      const { contacts, name, number } = this.state;
       return (
         <div>
-          <h1>Phone book</h1>
+          <h1>Phonebook</h1>
           <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              placeholder="Enter your name"
-              value={name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Add contact</button>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label>
+              Number:
+              <input
+                type="text"
+                name="number"
+                value={number}
+                onChange={this.handleChange}
+              />
+            </label>
+            <button type="submit">Add</button>
           </form>
           <h2>Contacts</h2>
           <ul>
             {contacts.map(contact => (
-              <li key={contact.id}>{contact.name}</li>
+              <li key={contact.id}>
+                {contact.name}: {contact.number}
+              </li>
             ))}
           </ul>
         </div>
